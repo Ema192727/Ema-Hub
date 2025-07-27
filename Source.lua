4a16d0153a07a036211254bfaccbb49813ba693a
Ema192727 <murillopereaemanuel@gmail.com>
2025-07-26 23:38:39 -0500

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -1,1 +1,2 @@
 https://www.roblox.com/es/games/102276247884041/Sonic-EXE-The-Disaster-REBORN
+loadstring(game:HttpGet('https://raw.githubusercontent.com/pascaldercoole1/Open-Source-Scripts/main/FreeIslands'))()
